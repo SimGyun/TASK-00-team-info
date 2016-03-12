b887e27d2db944462380eb4d5937b57ad9b52a91
morning <wzl_911@gamil.com>
2016-03-12 22:20:21 +0800

    chore: add node-sass, express, ejs and move index.html to /tpl feat: add morning.scss, main.scss, morning.html, app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lingchenxuan on 16/3/12.
+ */
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('views', __dirname + '/tpl');
+app.set( 'view engine', 'html' );
+app.engine( '.html', require( 'ejs' ).__express );
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+    res.render('./index');
+});
+
+app.listen(8080);
